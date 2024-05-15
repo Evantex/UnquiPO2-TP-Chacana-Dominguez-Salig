0ac628589e6a22cf79707a6f08a8411fa0f8aa00
ChacanaBruno <chacanabruno@gmail.com>
2024-05-15 14:17:49 -0600

    commit2 14:15

--- src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
+++ src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
@@ -2,11 +2,12 @@ package tp.po2.sem.sistemaEstacionamiento;
 
 public class SistemaEstacionamiento 
 {
-
+	private String nombre;
 	public static void main(String[] args)
 	{
 		// TODO Auto-generated method stub
 
 	}
 	//este es un commit 
+	// este es otro commit 14:15hs
 }
