46b33f88ff9c6f435e8f1e86684e2839372c7e63
Evantex <evantedev@gmail.com>
2024-05-15 13:36:57 -0300

    source folder test

---
+++ test/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamientoTest.java
@@ -0,0 +1,10 @@
+package tp.po2.sem.sistemaEstacionamiento;
+
+public class SistemaEstacionamientoTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
