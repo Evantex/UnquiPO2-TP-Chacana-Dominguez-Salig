05d38bfa29f29ab869d94d967c0b1226925735a1
ChacanaBruno <chacanabruno@gmail.com>
2024-05-15 18:11:55 -0600

    se agrego clase inspector e infracciones

---
+++ src/tp/po2/sem/inspector/Infraccion.java
@@ -0,0 +1,32 @@
+package tp.po2.sem.inspector;
+
+public class Infraccion {
+	private String patente;
+	private String infraccion;
+	private double montoInfraccion;
+
+	public String getPatente() {
+		return patente;
+	}
+
+	public void setPatente(String patente) {
+		this.patente = patente;
+	}
+
+	public String getInfraccion() {
+		return infraccion;
+	}
+
+	public void setInfraccion(String infraccion) {
+		this.infraccion = infraccion;
+	}
+
+	public double getMontoInfraccion() {
+		return montoInfraccion;
+	}
+
+	public void setMontoInfraccion(double montoInfraccion) {
+		this.montoInfraccion = montoInfraccion;
+	}
+
+}
--- src/tp/po2/sem/inspector/Inspector.java
+++ src/tp/po2/sem/inspector/Inspector.java
@@ -1,6 +1,28 @@
 package tp.po2.sem.inspector;
 
+import tp.po2.sem.sistemaEstacionamiento.SistemaEstacionamiento;
+
 public class Inspector {
-	private String nombreInspector; 
-	
+	private String nombreInspector;
+	private SistemaEstacionamiento sistemadeEstacionamientoMedido;
+
+	public String getNombreInspector() {
+		return nombreInspector;
+	}
+
+	public void setNombreInspector(String nombreInspector) {
+		this.nombreInspector = nombreInspector;
+	}
+
+	public SistemaEstacionamiento getSistemadeEstacionamientoMedido() {
+		return sistemadeEstacionamientoMedido;
+	}
+
+	public void setSistemadeEstacionamientoMedido(SistemaEstacionamiento sistemadeEstacionamientoMedido) {
+		this.sistemadeEstacionamientoMedido = sistemadeEstacionamientoMedido;
+	}
+
+	public boolean verificarPatente(String patente) {
+		return false;
+	}
 }
