14f5a13c2b95600fb8fccba1ab5a2a1ac8ce0c45
Evantex <evantedev@gmail.com>
2024-05-22 09:52:44 -0300

    Agrego modelado de app

--- src/tp/po2/sem/app/App.java
+++ src/tp/po2/sem/app/App.java
@@ -1,18 +1,26 @@
 package tp.po2.sem.app;
+import tp.po2.sem.sistemaEstacionamiento.*;
 
 public class App implements MovementSensor
 {
 	private Usuario usuarioAsociado;
 	private boolean deteccionDesplazamiento;
+	private SistemaEstacionamiento SEM;
+	private boolean vaCaminando;
 	
 	
 	
 	@Override
 	public void driving()
 	{
-		if( usuarioAsociado.estaEnZonaDeEstacionamiento() )
+		if( usuarioAsociado.estaEnZonaDeEstacionamiento() && this.vaCaminando )
+		{
+			this.vaCaminando = false;
+		}
 			
 	}
+	
+
 	
 	@Override
 	public void walking() {}
