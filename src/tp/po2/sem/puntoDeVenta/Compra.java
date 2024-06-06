4bb3fafc5422d4cdc18cd06ee75e6632871915ff
ChacanaBruno <chacanabruno@gmail.com>
2024-06-06 16:45:04 -0600

    desarrolo clase abstracta compra

--- src/tp/po2/sem/puntoDeVenta/Compra.java
+++ src/tp/po2/sem/puntoDeVenta/Compra.java
@@ -1,5 +1,52 @@
 package tp.po2.sem.puntoDeVenta;
 
-public class Compra {
+import java.time.LocalDateTime;
 
+public abstract class Compra {
+	private static int proximoNumeroDeControl = 1; // Variable estática para el número de control incremental
+
+	private PuntoDeVenta puntoDeVenta;
+	private String numeroDeControl;
+	
+	private LocalDateTime fechaHoraCompra;
+
+	// Constructor sin parámetros
+	public Compra() {
+		this.numeroDeControl = generarNumeroDeControl();
+		this.fechaHoraCompra = LocalDateTime.now();
+	}
+
+	// Constructor con parámetros
+	public Compra(PuntoDeVenta puntoDeVenta) {
+		this.puntoDeVenta = puntoDeVenta;
+		this.numeroDeControl = generarNumeroDeControl();
+		this.fechaHoraCompra = LocalDateTime.now();
+	}
+
+	// Método para generar el número de control incremental
+	private synchronized String generarNumeroDeControl() {
+		return String.valueOf(proximoNumeroDeControl++);
+	}
+
+	// Getters y Setters
+	public PuntoDeVenta getPuntoDeVenta() {
+		return puntoDeVenta;
+	}
+
+	public void setPuntoDeVenta(PuntoDeVenta puntoDeVenta) {
+		this.puntoDeVenta = puntoDeVenta;
+	}
+
+	public String getNumeroDeControl() {
+		return numeroDeControl;
+	}
+
+	public LocalDateTime getFechaHoraCompra() {
+		return fechaHoraCompra;
+	}
+
+	public void setFechaHoraCompra(LocalDateTime fechaHoraCompra) {
+		this.fechaHoraCompra = fechaHoraCompra;
+	}
 }
+
