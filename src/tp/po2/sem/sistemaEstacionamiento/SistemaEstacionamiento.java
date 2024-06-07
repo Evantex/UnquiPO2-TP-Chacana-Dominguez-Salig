b462dd20ae2afee9522c2a669af1af8085e27ad8
ChacanaBruno <chacanabruno@gmail.com>
2024-06-07 15:57:35 -0600

    constructor del sem actualizado

--- src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
+++ src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
@@ -1,10 +1,7 @@
-
 package tp.po2.sem.sistemaEstacionamiento;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
+import java.time.Duration;
 import java.util.HashSet;
 
 import tp.po2.sem.estacionamiento.Estacionamiento;
@@ -12,38 +9,29 @@ import tp.po2.sem.estacionamiento.EstacionamientoCompraPuntual;
 import tp.po2.sem.puntoDeVenta.CompraPuntual;
 
 public class SistemaEstacionamiento {
-	private Set<Estacionamiento> estacionamientos = HashSet<Estacionamiento>();
+    private Set<Estacionamiento> estacionamientos;
 
-	private Set<Integer> mutableSet = new HashSet<>();
+    public SistemaEstacionamiento(Set<Estacionamiento> estacionamientos) {
+        this.estacionamientos = estacionamientos;
+    }
 
-	public void registrarEstacionamiento(Estacionamiento unEstacionamiento) {
-		estacionamientos.add(unEstacionamiento);
+    public void registrarEstacionamiento(Estacionamiento unEstacionamiento) {
+        estacionamientos.add(unEstacionamiento);
+    }
 
-	}
+    public void registrarEstacionamientoCompraPuntual(String patente, Duration horasCompradas, CompraPuntual compraAsociada) {
+        EstacionamientoCompraPuntual estacionamiento = new EstacionamientoCompraPuntual(horasCompradas, patente, compraAsociada);
+        estacionamientos.add(estacionamiento);
+    }
 
-	public void registrarEstacionamientoCompraPuntual(String patente, Duration horasCompradas, CompraPuntual compraAsociada){
-		EstacionamientoCompraPuntual estacionamiento = new EstacionamientoCompraPuntual(horasCompradas, patente,
-				compraAsociada);
-		estacionamientos.add(estacionamiento);
-	}
+    public Integer getCantidadEstacionamientos() {
+        return estacionamientos.size();
+    }
 
-	public Integer getCantidadEstacionamientos() {
+    public boolean poseeEstacionamientoVigente(String patente) {
+        return true;
+    }
 
-		return estacionamientos.size();
-	}
-
-	public boolean poseeEstacionamientoVigente(String patente) {
-		return true;
-
-	}
-
-	 /*public void finalizarEstacionamiento( Estacionamiento estacionamiento )
-	{
-		this.estacionamientos.get
-	}*/
-
-	public void registrarInfraccion(String patente) {
-
-	}
-
+    public void registrarInfraccion(String patente) {
+    }
 }
--- test/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamientoTest.java
+++ test/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamientoTest.java
@@ -3,8 +3,8 @@ package tp.po2.sem.sistemaEstacionamiento;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -14,42 +14,37 @@ import tp.po2.sem.estacionamiento.Estacionamiento;
 
 public class SistemaEstacionamientoTest {
 
-	SistemaEstacionamiento sistemaEstacionamiento;
-	Estacionamiento estacionamientoMock;
-	List<Estacionamiento> spyListaEstacionamientos;
+    SistemaEstacionamiento sistemaEstacionamiento;
+    Estacionamiento estacionamientoMock;
+    Set<Estacionamiento> spyListaEstacionamientos;
 
-	@BeforeEach
-	public void setUp() {
-		
-		// Creamos el mock de Estacionamiento
-		estacionamientoMock = mock(Estacionamiento.class);
+    @BeforeEach
+    public void setUp() {
+        // Creamos el mock de Estacionamiento
+        estacionamientoMock = mock(Estacionamiento.class);
 
-		// Creamos un spy de la lista de estacionamientos
-		spyListaEstacionamientos = spy(new ArrayList<Estacionamiento>());
+        // Creamos un spy de la lista de estacionamientos
+        spyListaEstacionamientos = spy(new HashSet<Estacionamiento>());
 
-		// Creamos el SistemaEstacionamiento con el spy de la lista de estacionamientos
-		sistemaEstacionamiento = new SistemaEstacionamiento(spyListaEstacionamientos);
-	}
+        // Creamos el SistemaEstacionamiento con el spy de la lista de estacionamientos
+        sistemaEstacionamiento = new SistemaEstacionamiento(spyListaEstacionamientos);
+    }
 
-	@Test
-	public void testRegistrarUnEstacionamiento() {
-		
-		
-		// Ejecutamos el método que queremos probar, pasando el mock de Estacionamiento
-		sistemaEstacionamiento.registrarEstacionamiento(estacionamientoMock);
+    @Test
+    public void testRegistrarUnEstacionamiento() {
+        // Ejecutamos el método que queremos probar, pasando el mock de Estacionamiento
+        sistemaEstacionamiento.registrarEstacionamiento(estacionamientoMock);
 
-		// Con el spy verifico que el se le mando correctamente el mensaje add a la
-		// collecion dentro del mensaje getCantidadEstacionamientos().
-		
-		assertEquals(sistemaEstacionamiento.getCantidadEstacionamientos(), 1, 0);
+        // Con el spy verifico que se le mandó correctamente el mensaje add a la
+        // colección dentro del mensaje getCantidadEstacionamientos().
+        assertEquals(sistemaEstacionamiento.getCantidadEstacionamientos(), 1);
 
-		// Ademas se verifica que el envio de mensajes se haga en orden esperado.
-		InOrder orden = inOrder(spyListaEstacionamientos);
-		orden.verify(spyListaEstacionamientos).add(estacionamientoMock);
+        // Además se verifica que el envío de mensajes se haga en orden esperado.
+        InOrder orden = inOrder(spyListaEstacionamientos);
+        orden.verify(spyListaEstacionamientos).add(estacionamientoMock);
 
-		// Verifico con el spy que se le mando el mensaje size a la colleccion para
-		// obtener la cantidad de estacionamientos esperado.
-		verify(spyListaEstacionamientos).size();
-
-	}
+        // Verifico con el spy que se le mandó el mensaje size a la colección para
+        // obtener la cantidad de estacionamientos esperada.
+        verify(spyListaEstacionamientos).size();
+    }
 }
