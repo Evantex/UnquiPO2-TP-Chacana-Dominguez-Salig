c8c334690c2041d0d56a4435dba969bab97f84b5
ChacanaBruno <chacanabruno@gmail.com>
2024-06-10 18:43:40 -0600

    se realizo test cargarCelular

--- src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
+++ src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
@@ -14,50 +14,64 @@ import tp.po2.sem.inspector.Infraccion;
 import tp.po2.sem.inspector.Inspector;
 import tp.po2.sem.puntoDeVenta.CompraPuntual;
 
-public class SistemaEstacionamiento
-{
-    private Set<Estacionamiento> estacionamientos;
-    private HashMap<String, Double> saldoCelular = new HashMap<>();
-    
-    public void cargarCelular( String nroCelular, double saldo )
-    {
-    	saldoCelular.put(nroCelular, saldo);
-    }
+public class SistemaEstacionamiento {
+	private Set<Estacionamiento> estacionamientos;
+	private HashMap<String, Double> saldoCelular = new HashMap<>();
 
-    public SistemaEstacionamiento(Set<Estacionamiento> estacionamientos) {
-        this.estacionamientos = estacionamientos;
-    }
-
-    public void registrarEstacionamiento(Estacionamiento unEstacionamiento) {
-        estacionamientos.add(unEstacionamiento);
-    }
-
-    public void registrarEstacionamientoCompraPuntual(String patente, Duration horasCompradas, CompraPuntual compraAsociada) {
-        EstacionamientoCompraPuntual estacionamiento = new EstacionamientoCompraPuntual(horasCompradas, patente, compraAsociada);
-        estacionamientos.add(estacionamiento);
-    }
-
-    public Integer getCantidadEstacionamientos() {
-        return estacionamientos.size();
-    }
-
-    
-    public boolean poseeEstacionamientoVigente(String patente) {
-        return true;
-    }
-
-    public void registrarInfraccion(String patente, Inspector inspector) {
-    	
-    	LocalDateTime fechaYHoraActual = LocalDateTime.now();
-    	ZonaDeEstacionamiento zonaInspector = inspector.getZonaAsignada();
-    	Infraccion infraccion = new Infraccion(patente, fechaYHoraActual, inspector, zonaInspector);
-    	
-    	infracciones.add(infraccion);
-    	
-    }
-
-	public void realizarRecargaParaCelular(String numeroCelular, int saldo) {
-		// TODO Auto-generated method stub
-		
+	public SistemaEstacionamiento(Set<Estacionamiento> estacionamientos) {
+		this.estacionamientos = estacionamientos;
 	}
+
+	/**
+	 * Carga saldo a un número de celular. Si el celular ya tiene saldo, suma el
+	 * saldo nuevo al existente.
+	 *
+	 * @param nroCelular El número de celular al que se cargará el saldo.
+	 * @param saldo      El monto de saldo a cargar.
+	 */
+	public void cargarCelular(String nroCelular, double saldo) {
+		// Obtiene el saldo actual del número de celular o 0.0 si no existe,
+		// y luego suma el saldo nuevo al saldo actual.
+		saldoCelular.put(nroCelular, saldoCelular.getOrDefault(nroCelular, 0.0) + saldo);
+	}
+
+	/**
+	 * Obtiene el saldo actual de un número de celular.
+	 *
+	 * @param nroCelular El número de celular cuyo saldo se desea consultar.
+	 * @return El saldo actual del número de celular, o 0.0 si no existe.
+	 */
+	public double obtenerSaldoCelular(String nroCelular) {
+		return saldoCelular.getOrDefault(nroCelular, 0.0);
+	}
+
+	public void registrarEstacionamiento(Estacionamiento unEstacionamiento) {
+		estacionamientos.add(unEstacionamiento);
+	}
+
+	public void registrarEstacionamientoCompraPuntual(String patente, Duration horasCompradas,
+			CompraPuntual compraAsociada) {
+		EstacionamientoCompraPuntual estacionamiento = new EstacionamientoCompraPuntual(horasCompradas, patente,
+				compraAsociada);
+		estacionamientos.add(estacionamiento);
+	}
+
+	public Integer getCantidadEstacionamientos() {
+		return estacionamientos.size();
+	}
+
+	public boolean poseeEstacionamientoVigente(String patente) {
+		return true;
+	}
+
+	public void registrarInfraccion(String patente, Inspector inspector) {
+
+		LocalDateTime fechaYHoraActual = LocalDateTime.now();
+		ZonaDeEstacionamiento zonaInspector = inspector.getZonaAsignada();
+		Infraccion infraccion = new Infraccion(patente, fechaYHoraActual, inspector, zonaInspector);
+
+		infracciones.add(infraccion);
+
+	}
+
 }
--- test/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamientoTest.java
+++ test/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamientoTest.java
@@ -14,37 +14,83 @@ import tp.po2.sem.estacionamiento.Estacionamiento;
 
 public class SistemaEstacionamientoTest {
 
-    SistemaEstacionamiento sistemaEstacionamiento;
-    Estacionamiento estacionamientoMock;
-    Set<Estacionamiento> spyListaEstacionamientos;
+	SistemaEstacionamiento sistemaEstacionamiento;
+	Estacionamiento estacionamientoMock;
+	Set<Estacionamiento> spyListaEstacionamientos;
 
-    @BeforeEach
-    public void setUp() {
-        // Creamos el mock de Estacionamiento
-        estacionamientoMock = mock(Estacionamiento.class);
+	@BeforeEach
+	public void setUp() {
+		// Creamos el mock de Estacionamiento
+		estacionamientoMock = mock(Estacionamiento.class);
 
-        // Creamos un spy de la lista de estacionamientos
-        spyListaEstacionamientos = spy(new HashSet<Estacionamiento>());
+		// Creamos un spy de la lista de estacionamientos
+		spyListaEstacionamientos = spy(new HashSet<Estacionamiento>());
 
-        // Creamos el SistemaEstacionamiento con el spy de la lista de estacionamientos
-        sistemaEstacionamiento = new SistemaEstacionamiento(spyListaEstacionamientos);
-    }
+		// Creamos el SistemaEstacionamiento con el spy de la lista de estacionamientos
+		sistemaEstacionamiento = new SistemaEstacionamiento(spyListaEstacionamientos);
+	}
 
-    @Test
-    public void testRegistrarUnEstacionamiento() {
-        // Ejecutamos el método que queremos probar, pasando el mock de Estacionamiento
-        sistemaEstacionamiento.registrarEstacionamiento(estacionamientoMock);
+	@Test
+	public void testRegistrarUnEstacionamiento() {
+		// Ejecutamos el método que queremos probar, pasando el mock de Estacionamiento
+		sistemaEstacionamiento.registrarEstacionamiento(estacionamientoMock);
 
-        // Con el spy verifico que se le mandó correctamente el mensaje add a la
-        // colección dentro del mensaje getCantidadEstacionamientos().
-        assertEquals(sistemaEstacionamiento.getCantidadEstacionamientos(), 1);
+		// Con el spy verifico que se le mandó correctamente el mensaje add a la
+		// colección dentro del mensaje getCantidadEstacionamientos().
+		assertEquals(sistemaEstacionamiento.getCantidadEstacionamientos(), 1);
 
-        // Además se verifica que el envío de mensajes se haga en orden esperado.
-        InOrder orden = inOrder(spyListaEstacionamientos);
-        orden.verify(spyListaEstacionamientos).add(estacionamientoMock);
+		// Además se verifica que el envío de mensajes se haga en orden esperado.
+		InOrder orden = inOrder(spyListaEstacionamientos);
+		orden.verify(spyListaEstacionamientos).add(estacionamientoMock);
 
-        // Verifico con el spy que se le mandó el mensaje size a la colección para
-        // obtener la cantidad de estacionamientos esperada.
-        verify(spyListaEstacionamientos).size();
-    }
+		// Verifico con el spy que se le mandó el mensaje size a la colección para
+		// obtener la cantidad de estacionamientos esperada.
+		verify(spyListaEstacionamientos).size();
+	}
+
+	@Test
+	public void testCargarCelularNuevo() {
+		String nroCelular = "1234567890";
+		double saldo = 100.0;
+
+		// Cargar saldo por primera vez
+		sistemaEstacionamiento.cargarCelular(nroCelular, saldo);
+
+		// Verificar que el saldo se haya cargado correctamente
+		assertEquals(saldo, sistemaEstacionamiento.obtenerSaldoCelular(nroCelular), 0.01);
+	}
+
+	@Test
+	public void testCargarCelularExistente() {
+		String nroCelular = "1234567890";
+		double saldoInicial = 100.0;
+		double saldoAdicional = 50.0;
+
+		// Cargar saldo por primera vez
+		sistemaEstacionamiento.cargarCelular(nroCelular, saldoInicial);
+
+		// Cargar saldo adicional
+		sistemaEstacionamiento.cargarCelular(nroCelular, saldoAdicional);
+
+		// Verificar que el saldo se haya sumado correctamente
+		double saldoEsperado = saldoInicial + saldoAdicional;
+		assertEquals(saldoEsperado, sistemaEstacionamiento.obtenerSaldoCelular(nroCelular), 0.01);
+	}
+
+	@Test
+	public void testCargarCelularMultiplesVeces() {
+		String nroCelular = "1234567890";
+		double saldo1 = 50.0;
+		double saldo2 = 25.0;
+		double saldo3 = 75.0;
+
+		// Cargar saldo varias veces
+		sistemaEstacionamiento.cargarCelular(nroCelular, saldo1);
+		sistemaEstacionamiento.cargarCelular(nroCelular, saldo2);
+		sistemaEstacionamiento.cargarCelular(nroCelular, saldo3);
+
+		// Verificar que el saldo se haya sumado correctamente
+		double saldoEsperado = saldo1 + saldo2 + saldo3;
+		assertEquals(saldoEsperado, sistemaEstacionamiento.obtenerSaldoCelular(nroCelular), 0.01);
+	}
 }
