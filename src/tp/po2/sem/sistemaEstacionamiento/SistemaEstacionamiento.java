4e7aa945edf556418e77801dd1c6d33942c1595d
benja <benjamindominguezfdz@gmail.com>
2024-06-12 15:09:58 -0300

    se termina la logica de infracciones en SEM. Se modifico constructor SEM

--- src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
+++ src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -18,22 +19,17 @@ import tp.po2.sem.puntoDeVenta.CompraPuntual;
 public class SistemaEstacionamiento {
 	private Set<Estacionamiento> estacionamientos;
 	private HashMap<String, Double> saldoCelular = new HashMap<>();
-	private Set<Infraccion> infracciones;
+	private HashMap<String, List<Infraccion>> infraccionesPorPatente;
 	private LocalTime horaLaboralInicio;
 	private LocalTime horaLaboralFin;
 
-	public SistemaEstacionamiento(Set<Estacionamiento> estacionamientos, HashMap<String, Double> saldoCelular,
-			Set<Infraccion> infracciones) {
+	public SistemaEstacionamiento(Set<Estacionamiento> estacionamientos) {
 		super();
 		this.estacionamientos = estacionamientos;
-		this.saldoCelular = saldoCelular;
-		this.infracciones = infracciones;
+		this.saldoCelular = new HashMap<>();
+		this.infraccionesPorPatente = new HashMap<>();
 		this.horaLaboralInicio = LocalTime.of(7, 0); // 7:00 AM
 		this.horaLaboralFin = LocalTime.of(20, 0); // 8:00 PM
-	}
-
-	public SistemaEstacionamiento(Set<Estacionamiento> estacionamientos) {
-		this.estacionamientos = estacionamientos;
 	}
 
 	/**
@@ -79,14 +75,29 @@ public class SistemaEstacionamiento {
 	}
 
 	public void registrarInfraccion(String patente, Inspector inspector) {
+		
+        LocalDateTime fechaYHoraActual = LocalDateTime.now();
+        ZonaDeEstacionamiento zonaInspector = inspector.getZonaAsignada();
+        Infraccion infraccion = new Infraccion(patente, fechaYHoraActual, inspector, zonaInspector);
 
-		LocalDateTime fechaYHoraActual = LocalDateTime.now();
-		ZonaDeEstacionamiento zonaInspector = inspector.getZonaAsignada();
-		Infraccion infraccion = new Infraccion(patente, fechaYHoraActual, inspector, zonaInspector);
+        // Obtener la lista de infracciones asociada con la patente
+        // El .get es un metodo por default del hashmap que te trae el value asociada a esa key
+        List<Infraccion> infracciones = infraccionesPorPatente.get(patente);
 
-		infracciones.add(infraccion);
+        // Si no tenia infracciones, se crea una nueva lista de infracciones y la agrega al HashMap
+        if (infracciones == null) {
+            infracciones = new ArrayList<>();
+            infraccionesPorPatente.put(patente, infracciones);
+        }
 
-	}
+        // Agrega la infracción a la lista si ya tenia infracciones anteriores.
+        infracciones.add(infraccion);
+    }
+
+    // Método para obtener las infracciones por patente
+    public List<Infraccion> obtenerInfraccionesDeLaPatente(String patente) {
+        return infraccionesPorPatente.get(patente);
+    }
 
 	public Set<Estacionamiento> getEstacionamientos() {
 		return estacionamientos;
