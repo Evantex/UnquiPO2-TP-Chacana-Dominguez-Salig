9b073bd8e2198899df167ad6b4525bf4e534c1b8
ChacanaBruno <chacanabruno@gmail.com>
2024-06-14 15:38:14 -0600

    el sem tiene una lista de compras mas accesible

--- src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
+++ src/tp/po2/sem/sistemaEstacionamiento/SistemaEstacionamiento.java
@@ -15,147 +15,34 @@ import tp.po2.sem.estacionamiento.Estacionamiento;
 import tp.po2.sem.estacionamiento.EstacionamientoCompraPuntual;
 import tp.po2.sem.inspector.Infraccion;
 import tp.po2.sem.inspector.Inspector;
+import tp.po2.sem.puntoDeVenta.Compra;
 import tp.po2.sem.puntoDeVenta.CompraPuntual;
 
 public class SistemaEstacionamiento {
 	private Set<Estacionamiento> estacionamientos;
 	private HashMap<String, Double> saldoCelular;
 	private HashMap<String, List<Infraccion>> infraccionesPorPatente;
+	private Set<Compra> comprasPuntoDeVenta;
 	private LocalTime horaLaboralInicio;
 	private LocalTime horaLaboralFin;
 	private Notificador sistemaAlertas;
 
-	public SistemaEstacionamiento() 
-	{
+	public SistemaEstacionamiento() {
 		super();
 		this.estacionamientos = new HashSet<>();
 		this.saldoCelular = new HashMap<>();
 		this.infraccionesPorPatente = new HashMap<>();
+		this.comprasPuntoDeVenta = new HashSet<>();
 		this.horaLaboralInicio = LocalTime.of(7, 0); // 7:00 AM
 		this.horaLaboralFin = LocalTime.of(20, 0); // 8:00 PM
 		this.setSistemaAlertas(new Notificador());
 	}
-	
-	
-	/**
-	 * Carga saldo a un número de celular. Si el celular ya tiene saldo, suma el
-	 * saldo nuevo al existente.
-	 *
-	 * @param nroCelular El número de celular al que se cargará el saldo.
-	 * @param saldo      El monto de saldo a cargar.
-	 */
-	public void cargarCelular(String nroCelular, double saldo)
-	{
-		// Obtiene el saldo actual del número de celular o 0.0 si no existe,
-		// y luego suma el saldo nuevo al saldo actual.
-		saldoCelular.put(nroCelular, saldoCelular.getOrDefault(nroCelular, 0.0) + saldo);
-	}
 
-	/**
-	 * Obtiene el saldo actual de un número de celular.
-	 *
-	 * @param nroCelular El número de celular cuyo saldo se desea consultar.
-	 * @return El saldo actual del número de celular, o 0.0 si no existe.
-	 */
-	
-	public double obtenerSaldoCelular(String nroCelular)
-	{
-		this.verificarSiExisteCelular(nroCelular);
-		return this.saldoCelular.get(nroCelular);
-	}
-
-	private void verificarSiExisteCelular( String nroCelular )
-	{
-		if( !this.saldoCelular.containsKey(nroCelular) )
-		{
-			this.agregarCelular(nroCelular);
-		}
-	}
-	
-	public void agregarCelular( String nroCelular )
-	{
-		this.saldoCelular.put(nroCelular, 0.0);
-	}
-	
-	public void registrarEstacionamiento(Estacionamiento unEstacionamiento) {
-		estacionamientos.add(unEstacionamiento);
-	}
-
-	public void registrarEstacionamientoCompraPuntual(String patente, Duration horasCompradas,
-			CompraPuntual compraAsociada, String nombreZonaEstacionamiento) {
-		EstacionamientoCompraPuntual estacionamiento = new EstacionamientoCompraPuntual(horasCompradas, patente,
-				 compraAsociada);
-		estacionamientos.add(estacionamiento);
-		
-	}
-
-	public Integer getCantidadEstacionamientos() {
-		return estacionamientos.size();
-	}
-	
-	//metodo que utiliza el Inspector, este debe consultar por patente
-	public boolean poseeEstacionamientoVigente(String patente) {
-		
-		//dada la patente: filtro para que me retorne el identificador de estacionamiento asociada a esa patente
-		//de ese modo puedo utilizar el metodo estaVigente()
-	    Optional<String> identificadorEstacionamientoOptional = estacionamientos.stream()
-	            .filter(e -> e.getPatente().equals(patente))
-	            .map(e -> String.valueOf(e.getIdentificadorEstacionamiento()))
-	            .findFirst();
-
-	    String identificadorEstacionamiento = identificadorEstacionamientoOptional.orElse(null);
-	    return estaVigente(identificadorEstacionamiento);
-	}
-	
-	public boolean estaVigente(String identificadorEstacionamiento) 
-	{
-	    return this.estacionamientos.stream()
-	            .anyMatch(e -> e.estaVigente() && e.getIdentificadorEstacionamiento().equals(identificadorEstacionamiento));
-	}
-
-	public void registrarInfraccion(String patente, Inspector inspector) {
-		
-        LocalDateTime fechaYHoraActual = LocalDateTime.now();
-        ZonaDeEstacionamiento zonaInspector = inspector.getZonaAsignada();
-        Infraccion infraccion = new Infraccion(patente, fechaYHoraActual, inspector, zonaInspector);
-
-        // Obtener la lista de infracciones asociada con la patente
-        // El .get es un metodo por default del hashmap que te trae el value asociada a esa key
-        List<Infraccion> infracciones = infraccionesPorPatente.get(patente);
-
-        // Si no tenia infracciones, se crea una nueva lista de infracciones y la agrega al HashMap
-        if (infracciones == null) {
-            infracciones = new ArrayList<>();
-            infraccionesPorPatente.put(patente, infracciones);
-        }
-
-        // Agrega la infracción a la lista si ya tenia infracciones anteriores.
-        infracciones.add(infraccion);
-    }
-
-    // Método para obtener las infracciones por patente
-    public List<Infraccion> obtenerInfraccionesDeLaPatente(String patente) {
-        return infraccionesPorPatente.get(patente);
-    }
-
-	public Set<Estacionamiento> getEstacionamientos() {
-		return estacionamientos;
-	}
-
-	public void setEstacionamientos(Set<Estacionamiento> estacionamientos) {
-		this.estacionamientos = estacionamientos;
-	}
-
-	/*
-	public HashMap<String, Double> getSaldoCelular() {
-		return saldoCelular;
-	}
-	*/
+	// gettes and setters
 
 	public void setSaldoCelular(HashMap<String, Double> saldoCelular) {
 		this.saldoCelular = saldoCelular;
 	}
-
 
 	public LocalTime getHoraLaboralInicio() {
 		return horaLaboralInicio;
@@ -172,33 +59,154 @@ public class SistemaEstacionamiento {
 	public void setHoraLaboralFin(LocalTime horaLaboralFin) {
 		this.horaLaboralFin = horaLaboralFin;
 	}
-	
-	
-	public void finalizarEstacionamiento(String identificadorEstacionamiento)
-	{
-	    this.estacionamientos.stream()
-	        .filter(e -> e.estaVigente() && e.getIdentificadorEstacionamiento().equals(identificadorEstacionamiento))
-	        .findAny()
-	        .ifPresent(estacionamiento -> estacionamiento.setFinEstacionamiento());
+
+	public Set<Estacionamiento> getEstacionamientos() {
+		return estacionamientos;
+	}
+
+	public void setEstacionamientos(Set<Estacionamiento> estacionamientos) {
+		this.estacionamientos = estacionamientos;
+	}
+
+	public HashMap<String, List<Infraccion>> getInfraccionesPorPatente() {
+		return infraccionesPorPatente;
+	}
+
+	public void setInfraccionesPorPatente(HashMap<String, List<Infraccion>> infraccionesPorPatente) {
+		this.infraccionesPorPatente = infraccionesPorPatente;
+	}
+
+	public Set<Compra> getComprasPuntoDeVenta() {
+		return comprasPuntoDeVenta;
+	}
+
+	public void setComprasPuntoDeVenta(Set<Compra> comprasPuntoDeVenta) {
+		this.comprasPuntoDeVenta = comprasPuntoDeVenta;
+	}
+
+	public HashMap<String, Double> getSaldoCelular() {
+		return saldoCelular;
+	}
+
+	public Integer getCantidadEstacionamientos() {
+		return estacionamientos.size();
+	}
+
+	// registraciones
+
+	public void registrarCompra(Compra compra) {
+
+		comprasPuntoDeVenta.add(compra);
+	}
+
+	public void registrarEstacionamiento(Estacionamiento unEstacionamiento) {
+		estacionamientos.add(unEstacionamiento);
+	}
+
+	public void registrarEstacionamientoCompraPuntual(String patente, Duration horasCompradas,
+			CompraPuntual compraAsociada, String nombreZonaEstacionamiento) {
+		EstacionamientoCompraPuntual estacionamiento = new EstacionamientoCompraPuntual(horasCompradas, patente,
+				compraAsociada);
+		estacionamientos.add(estacionamiento);
+
+	}
+
+	// CAMBIAR RECARGAS
+
+	public void cargarCelular(String nroCelular, double saldo) {
+		// Obtiene el saldo actual del número de celular o 0.0 si no existe,
+		// y luego suma el saldo nuevo al saldo actual.
+		saldoCelular.put(nroCelular, saldoCelular.getOrDefault(nroCelular, 0.0) + saldo);
+	}
+
+	public double obtenerSaldoCelular(String nroCelular) {
+		this.verificarSiExisteCelular(nroCelular);
+		return this.saldoCelular.get(nroCelular);
+	}
+
+	private void verificarSiExisteCelular(String nroCelular) {
+		if (!this.saldoCelular.containsKey(nroCelular)) {
+			this.agregarCelular(nroCelular);
+		}
+	}
+
+	public void agregarCelular(String nroCelular) {
+		this.saldoCelular.put(nroCelular, 0.0);
+	}
+
+	// Logica Inspector
+
+	// metodo que utiliza el Inspector, este debe consultar por patente
+	public boolean poseeEstacionamientoVigente(String patente) {
+
+		// dada la patente: filtro para que me retorne el identificador de
+		// estacionamiento asociada a esa patente
+		// de ese modo puedo utilizar el metodo estaVigente()
+		Optional<String> identificadorEstacionamientoOptional = estacionamientos.stream()
+				.filter(e -> e.getPatente().equals(patente))
+				.map(e -> String.valueOf(e.getIdentificadorEstacionamiento())).findFirst();
+
+		String identificadorEstacionamiento = identificadorEstacionamientoOptional.orElse(null);
+		return estaVigente(identificadorEstacionamiento);
+	}
+
+	public boolean estaVigente(String identificadorEstacionamiento) {
+		return this.estacionamientos.stream().anyMatch(
+				e -> e.estaVigente() && e.getIdentificadorEstacionamiento().equals(identificadorEstacionamiento));
+	}
+
+	public void registrarInfraccion(String patente, Inspector inspector) {
+
+		LocalDateTime fechaYHoraActual = LocalDateTime.now();
+		ZonaDeEstacionamiento zonaInspector = inspector.getZonaAsignada();
+		Infraccion infraccion = new Infraccion(patente, fechaYHoraActual, inspector, zonaInspector);
+
+		// Obtener la lista de infracciones asociada con la patente
+		// El .get es un metodo por default del hashmap que te trae el value asociada a
+		// esa key
+		List<Infraccion> infracciones = infraccionesPorPatente.get(patente);
+
+		// Si no tenia infracciones, se crea una nueva lista de infracciones y la agrega
+		// al HashMap
+		if (infracciones == null) {
+			infracciones = new ArrayList<>();
+			infraccionesPorPatente.put(patente, infracciones);
+		}
+
+		// Agrega la infracción a la lista si ya tenia infracciones anteriores.
+		infracciones.add(infraccion);
+	}
+
+	// Método para obtener las infracciones por patente
+	public List<Infraccion> obtenerInfraccionesDeLaPatente(String patente) {
+		return infraccionesPorPatente.get(patente);
+	}
+
+	/*
+	 * public HashMap<String, Double> getSaldoCelular() { return saldoCelular; }
+	 */
+
+	public void finalizarEstacionamiento(String identificadorEstacionamiento) {
+		this.estacionamientos.stream().filter(
+				e -> e.estaVigente() && e.getIdentificadorEstacionamiento().equals(identificadorEstacionamiento))
+				.findAny().ifPresent(estacionamiento -> estacionamiento.setFinEstacionamiento());
 	}
 
 	public Estacionamiento getEstacionamiento(String identificadorEstacionamiento) throws Exception {
-	    return this.estacionamientos.stream()
-	            .filter(e -> e.estaVigente() && e.getIdentificadorEstacionamiento().equals(identificadorEstacionamiento))
-	            .findAny()
-	            .orElseThrow(() -> new Exception("El estacionamiento no existe o no está vigente"));
+		return this.estacionamientos.stream().filter(
+				e -> e.estaVigente() && e.getIdentificadorEstacionamiento().equals(identificadorEstacionamiento))
+				.findAny().orElseThrow(() -> new Exception("El estacionamiento no existe o no está vigente"));
 	}
-	
-	
+
 	// LOGICA DE OBSERVER
 	public void agregarObservador(Observer observador) {
-        getSistemaAlertas().agregarObservador(observador);
-    }
+		getSistemaAlertas().agregarObservador(observador);
+	}
 
-    public void eliminarObservador(Observer observador) {
-        getSistemaAlertas().eliminarObservador(observador);
-    }
-	
+	public void eliminarObservador(Observer observador) {
+		getSistemaAlertas().eliminarObservador(observador);
+	}
+
 	public Notificador getSistemaAlertas() {
 		return sistemaAlertas;
 	}
@@ -208,15 +216,16 @@ public class SistemaEstacionamiento {
 	}
 
 	public void notificarSistemaAlertasFinEstacionamiento(String patente, String celular) {
-		
-		sistemaAlertas.notificarObservadores(new EventoEstacionamiento(EventoEstacionamiento.Tipo.FIN, patente, celular));
-		
+
+		sistemaAlertas
+				.notificarObservadores(new EventoEstacionamiento(EventoEstacionamiento.Tipo.FIN, patente, celular));
+
 	}
 
 	public void notificarSistemaAlertasInicioEstacionamiento(String patente, String celular) {
-		sistemaAlertas.notificarObservadores(new EventoEstacionamiento(EventoEstacionamiento.Tipo.INICIO, patente, celular));
-		
+		sistemaAlertas
+				.notificarObservadores(new EventoEstacionamiento(EventoEstacionamiento.Tipo.INICIO, patente, celular));
+
 	}
-	
-	
+
 }
