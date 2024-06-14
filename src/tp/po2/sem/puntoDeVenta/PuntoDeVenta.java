142474267b187de0e34c9feb6fed28ac84fbd9b1
ChacanaBruno <chacanabruno@gmail.com>
2024-06-14 15:57:07 -0600

    la responsabilidad del punto de venta solo es acreditar/crear las compras, ya no almacena compras

--- src/tp/po2/sem/puntoDeVenta/PuntoDeVenta.java
+++ src/tp/po2/sem/puntoDeVenta/PuntoDeVenta.java
@@ -9,19 +9,17 @@ import tp.po2.sem.estacionamiento.Estacionamiento;
 import tp.po2.sem.estacionamiento.EstacionamientoCompraPuntual;
 import tp.po2.sem.sistemaEstacionamiento.SistemaEstacionamiento;
 
-public class PuntoDeVenta {
+public class PuntoDeVenta { // responsabilidad de generar objetos compras
 	private String identificadorPuntoDeVenta;
 	private SistemaEstacionamiento sem;
-	private Set<Compra> setDeCompras;
 	private ZonaDeEstacionamiento zona;
 
-	public PuntoDeVenta(String identificadorPuntoDeVenta, SistemaEstacionamiento sem, Set<Compra> setDeCompras,
-			ZonaDeEstacionamiento zona) {
+	public PuntoDeVenta(String identificadorPuntoDeVenta, SistemaEstacionamiento sem, ZonaDeEstacionamiento zona) {
 		super();
 		this.identificadorPuntoDeVenta = identificadorPuntoDeVenta;
 		this.sem = sem;
 		this.zona = zona;
-		this.setDeCompras = setDeCompras;
+
 	}
 
 	public ZonaDeEstacionamiento getZona() {
@@ -48,33 +46,16 @@ public class PuntoDeVenta {
 		this.sem = sem;
 	}
 
-	public Set<Compra> getSetDeCompras() {
-		return setDeCompras;
-	}
-
-	public void setSetDeCompras(Set<Compra> setDeCompras) {
-		this.setDeCompras = setDeCompras;
-	}
-
-	public void registrarEstacionamiento(String patente, Duration cantidadDeHoras) {
-		try {
-
-			this.verificarHorarioPermitido();
-			this.verificarSiLaCantidadDeHorasEsValidaEnEsteHorario(cantidadDeHoras);
-
+	public void registrarEstacionamientoCompraPuntual(String patente, Duration cantidadDeHoras) {
+	
 			CompraPuntual compraPuntual = new CompraPuntual(this, cantidadDeHoras);
-			String IdentificardorDeZonaDelPuntoDeVenta = this.getZona().getIdentificardorDeZona(); // esto esta de mas?
 
-			sem.registrarEstacionamientoCompraPuntual(patente, cantidadDeHoras, compraPuntual,
-					IdentificardorDeZonaDelPuntoDeVenta);
+			sem.registrarEstacionamientoCompraPuntual(patente, cantidadDeHoras, compraPuntual);
 
-			setDeCompras.add(compraPuntual);
-		} catch (Exception e) {
-			// Imprimir el mensaje de la excepción
-			System.out.println("Ocurrió un error: " + e.getMessage());
-		}
+			sem.registrarCompra(compraPuntual);
+	
 	}
-
+/*
 	private void verificarSiLaCantidadDeHorasEsValidaEnEsteHorario(Duration cantidadDeHoras) throws Exception {
 		if (!this.esValidaLaCantidadDeHorasEnEsteMomento(cantidadDeHoras)) {
 			throw new Exception("No es valida la cantidad de horas solicitada en este momento");
@@ -84,7 +65,7 @@ public class PuntoDeVenta {
 	private boolean esValidaLaCantidadDeHorasEnEsteMomento(Duration cantidadDeHoras) {
 		LocalTime horaActual = LocalTime.now();
 		LocalTime horaFinAlquiler = horaActual.plus(cantidadDeHoras);
-		
+
 		// Verifica si la hora de fin del alquiler es antes o igual a la hora de cierre
 		return !horaFinAlquiler.isAfter(sem.getHoraLaboralFin());
 	}
@@ -101,7 +82,7 @@ public class PuntoDeVenta {
 		LocalTime horaMaxima = sem.getHoraLaboralFin();
 
 		return (horaActual.isAfter(horaMinima)) && (horaActual.isBefore(horaMaxima));
-	}
+	}*/
 
 	public void cargarSaldoEnCelular(String numeroCelular, double saldo) {
 		CompraRecargaCelular CompraRecargaCelular = new CompraRecargaCelular(this, numeroCelular, saldo);
@@ -110,9 +91,6 @@ public class PuntoDeVenta {
 		setDeCompras.add(CompraRecargaCelular);
 	}
 
-	public int getCantidadCompras() {
 
-		return setDeCompras.size();
-	}
 
 }
