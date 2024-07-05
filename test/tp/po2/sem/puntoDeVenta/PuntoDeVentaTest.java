d4aa4067ae953f709cbc4104a549e85648729c96
ChacanaBruno <chacanabruno@gmail.com>
2024-07-04 18:37:33 -0600

    test 100%

--- test/tp/po2/sem/puntoDeVenta/PuntoDeVentaTest.java
+++ test/tp/po2/sem/puntoDeVenta/PuntoDeVentaTest.java
@@ -1,200 +1,105 @@
 package tp.po2.sem.puntoDeVenta;
 
 import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
-
-import org.mockito.MockitoAnnotations;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.LinkedHashSet;
-import java.util.List;
-import java.util.Set;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.spy;
-
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import tp.po2.sem.ZonaDeEstacionamiento.ZonaDeEstacionamiento;
-import tp.po2.sem.estacionamiento.Estacionamiento;
 import tp.po2.sem.sistemaEstacionamiento.SistemaEstacionamiento;
 
-class PuntoDeVentaTest {
-	PuntoDeVenta puntoDeVentaSUT;
-	SistemaEstacionamiento sistemaEstacionamientoMock;
-	Estacionamiento estacionamientoMock;
-	CompraPuntual compraDeEstacionamientoMock;
-	ZonaDeEstacionamiento zonaMock;
-	List<Estacionamiento> spyListaEstacionamientos;
-	Set<Compra> spySetDeCompras;
-	@Captor
-	private ArgumentCaptor<CompraPuntual> compraPuntualCaptor;
-	@Captor
-	private ArgumentCaptor<CompraRecargaCelular> compraCelularCaptor;
+public class PuntoDeVentaTest {
+
+	private PuntoDeVenta puntoDeVenta;
+	private SistemaEstacionamiento sistemaEstacionamientoMock;
+	private ZonaDeEstacionamiento zonaDeEstacionamientoMock;
+	private LocalDate fechaCompraMock;
 
 	@BeforeEach
 	public void setUp() {
-
-		MockitoAnnotations.openMocks(this); // INICIALIZO LOS MOCK "CAPTOR"
-
-		estacionamientoMock = mock(Estacionamiento.class);
-		zonaMock = mock(ZonaDeEstacionamiento.class);
-		compraDeEstacionamientoMock = mock(CompraPuntual.class);
 		sistemaEstacionamientoMock = mock(SistemaEstacionamiento.class);
-		spyListaEstacionamientos = spy(new ArrayList<Estacionamiento>());
-		spySetDeCompras = spy(new LinkedHashSet<Compra>());
-		puntoDeVentaSUT = new PuntoDeVenta("identificador", sistemaEstacionamientoMock, zonaMock);
-
-		// Configurar horarios válidos
-		when(sistemaEstacionamientoMock.getHoraLaboralInicio()).thenReturn(LocalTime.of(7, 0));
-		when(sistemaEstacionamientoMock.getHoraLaboralFin()).thenReturn(LocalTime.of(20, 0));
+		zonaDeEstacionamientoMock = mock(ZonaDeEstacionamiento.class);
+		puntoDeVenta = new PuntoDeVenta("PV001", sistemaEstacionamientoMock, zonaDeEstacionamientoMock);
+		fechaCompraMock = LocalDate.of(2024, 7, 4);
 	}
 
-	// TEST DE GETTERS Y SETTERS
+	@Test
+	public void testRegistrarEstacionamientoCompraPuntual() throws Exception {
+		// Configurar el comportamiento de sistemaEstacionamientoMock.puedeEstacionar()
+		String patente = "ABC123";
+		LocalTime horaInicio = LocalTime.now();
+		Duration cantidadDeHoras = Duration.ofHours(2);
+
+		// Configurado para cuando se llame a puedeEstacionar con los siguientes
+		// argumentos,
+		// no haga nada (do nothing).
+		doNothing().when(sistemaEstacionamientoMock).puedeEstacionar(eq(patente), any(LocalTime.class),
+				any(LocalTime.class));
+
+		// Llamar al método que queremos probar
+		puntoDeVenta.registrarEstacionamientoCompraPuntual(patente, fechaCompraMock, cantidadDeHoras);
+
+		// Verificar que sistemaEstacionamientoMock.puedeEstacionar() fue llamado con
+		// los argumentos esperados
+		verify(sistemaEstacionamientoMock).puedeEstacionar(eq(patente), any(LocalTime.class), any(LocalTime.class));
+		verify(sistemaEstacionamientoMock).solicitudDeEstacionamientoCompraPuntual(eq(patente),
+				any(CompraPuntual.class));
+	}
+
+	@Test
+	public void testCargarSaldoEnCelular() {
+		// Arrange
+		String numeroCelular = "123456789";
+		double saldo = 50.0;
+
+		// Act
+		puntoDeVenta.cargarSaldoEnCelular(numeroCelular, saldo);
+
+		// Assert
+		verify(sistemaEstacionamientoMock).cargarCelular(eq(numeroCelular), eq(saldo));
+		verify(sistemaEstacionamientoMock).registrarCompra(any(CompraRecargaCelular.class));
+	}
 
 	@Test
 	public void testGetZona() {
-		ZonaDeEstacionamiento zonaEsperada = puntoDeVentaSUT.getZona();
-		assertEquals(zonaMock, zonaEsperada);
+		assertEquals(zonaDeEstacionamientoMock, puntoDeVenta.getZona());
 	}
 
 	@Test
 	public void testSetZona() {
 		ZonaDeEstacionamiento nuevaZona = mock(ZonaDeEstacionamiento.class);
-		puntoDeVentaSUT.setZona(nuevaZona);
-		assertEquals(nuevaZona, puntoDeVentaSUT.getZona());
+		puntoDeVenta.setZona(nuevaZona);
+		assertEquals(nuevaZona, puntoDeVenta.getZona());
 	}
 
 	@Test
 	public void testGetIdentificadorPuntoDeVenta() {
-		String identificador = puntoDeVentaSUT.getIdentificadorPuntoDeVenta();
-		assertEquals("identificador", identificador);
+		assertEquals("PV001", puntoDeVenta.getIdentificadorPuntoDeVenta());
 	}
 
 	@Test
 	public void testSetIdentificadorPuntoDeVenta() {
-		puntoDeVentaSUT.setIdentificadorPuntoDeVenta("PV2");
-		assertEquals("PV2", puntoDeVentaSUT.getIdentificadorPuntoDeVenta());
+		String nuevoIdentificador = "PV002";
+		puntoDeVenta.setIdentificadorPuntoDeVenta(nuevoIdentificador);
+		assertEquals(nuevoIdentificador, puntoDeVenta.getIdentificadorPuntoDeVenta());
 	}
 
 	@Test
 	public void testGetSem() {
-		SistemaEstacionamiento sem = puntoDeVentaSUT.getSem();
-		assertEquals(sistemaEstacionamientoMock, sem);
+		assertEquals(sistemaEstacionamientoMock, puntoDeVenta.getSem());
 	}
 
 	@Test
 	public void testSetSem() {
-		SistemaEstacionamiento nuevoSem = mock(SistemaEstacionamiento.class);
-		puntoDeVentaSUT.setSem(nuevoSem);
-		assertEquals(nuevoSem, puntoDeVentaSUT.getSem());
+		SistemaEstacionamiento nuevoSistema = mock(SistemaEstacionamiento.class);
+		puntoDeVenta.setSem(nuevoSistema);
+		assertEquals(nuevoSistema, puntoDeVenta.getSem());
 	}
 
-// TESTS LOGICA DE REGISTRAR COMPRAS
-
-	@Test
-	public void testRegistrarEstacionamientoCompraPuntual_Valido() throws Exception {
-		Duration cantidadDeHoras = Duration.ofHours(2);
-		when(sistemaEstacionamientoMock.esValidoRegistrarEstacionamiento(cantidadDeHoras)).thenReturn(true);
-
-		puntoDeVentaSUT.registrarEstacionamientoCompraPuntual("ABC123", cantidadDeHoras);
-
-		// Verificacion: se mandaron los mensajes al SEM
-		verify(sistemaEstacionamientoMock).registrarEstacionamientoCompraPuntual(eq("ABC123"), eq(cantidadDeHoras),
-				compraPuntualCaptor.capture());
-		verify(sistemaEstacionamientoMock).registrarCompra(compraPuntualCaptor.capture());
-
-		// Verificacion: se creó una instancia de CompraPuntual
-		CompraPuntual compraPuntual = compraPuntualCaptor.getValue();
-		assertNotNull(compraPuntual);
-		assertEquals(cantidadDeHoras, compraPuntual.getHorasCompradas());
-	}
-
-	@Test
-	public void testNoSePuedeRegistrarUnEstacionamientoEnUnHorarioNoValido() throws Exception {
-
-		when(sistemaEstacionamientoMock.esHorarioLaboral()).thenReturn(false);
-
-		Duration cantidadDeHoras = Duration.ofHours(2);
-
-		Exception exception = assertThrows(Exception.class, () -> {
-			puntoDeVentaSUT.registrarEstacionamientoCompraPuntual("Patente", cantidadDeHoras);
-		});
-
-		// Verificar el mensaje de la excepción
-		assertEquals("No es un horario apto para procesar el estacionamiento", exception.getMessage());
-
-		// Verificar que no se llamaron a los metodos esperados del mock
-		verify(sistemaEstacionamientoMock, never()).registrarEstacionamientoCompraPuntual(anyString(),
-				any(Duration.class), any(CompraPuntual.class));
-		verify(sistemaEstacionamientoMock, never()).registrarCompra(any(CompraPuntual.class));
-	}
-
-	@Test
-	public void testRegistrarUnaRecargaDeSaldoEnPuntoDeVenta() {
-		// Datos de prueba
-		String numeroCelular = "123456789";
-		double saldo = 50.0;
-
-		// Llamar al método bajo prueba
-		puntoDeVentaSUT.cargarSaldoEnCelular(numeroCelular, saldo);
-
-		// Verificar que se llamaron los métodos esperados en el mock
-		verify(sistemaEstacionamientoMock).cargarCelular(numeroCelular, saldo);
-		verify(sistemaEstacionamientoMock).registrarCompra(compraCelularCaptor.capture());
-
-		// Verifico la instancia correcta de compra
-		CompraRecargaCelular compraRecarga = compraCelularCaptor.getValue();
-		assertNotNull(compraRecarga);
-		assertEquals(saldo, compraRecarga.getMontoSaldo());
-	}
-
-	@Test
-	public void registrarUnEstacionamientoAntesDeLaHoraInicio() {
-		String patente = "123";
-		LocalDate fechaCompra = LocalDate.now();
-		LocalTime horaInicio = LocalTime.of(6, 0);
-		LocalTime horaFin = LocalTime.of(10, 0);
-
-		puntoDeVentaSUT.registrarEstacionamientoCompraPuntual(patente, fechaCompra, horaInicio, horaFin);
-		
-		
-		
-		
-	}
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 }
